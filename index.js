138022a5307826f70b175ae427398c3ea87b4d98
jaison alievi <jaison.alieve@gmail.com>
2021-03-01 21:30:06 -0300

    site alievis do zero

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const btn = document.querySelector(".btn")
+const drop = document.querySelector(".dropdown")
+
+btn.addEventListener('click', () => {
+  drop.classList.toggle("hidden")
+})
+
+window.addEventListener("click", (event) => {
+  if(event.target !== btn) {
+    drop.classList.add("hidden")
+  }
+})
+
+btn.addEventListener('click', () => {
+  if(drop.getAttribute() === "hidden") {
+    drop.classList.remove("hidden")
+  }
+})
